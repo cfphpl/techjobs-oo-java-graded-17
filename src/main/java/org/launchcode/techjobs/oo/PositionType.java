c5700633cc925a875616a1be3b23ab9353d1bf45
Kevin <cfphpl@gmail.com>
2023-10-16 17:54:12 -0500

    first

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
 public class PositionType {
 
     private int id;
@@ -17,9 +18,29 @@ public class PositionType {
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
+    public PositionType(String value) {
+        this();
+        this.value = value;
+    }
+
+    public String toString() {
+        return value;
+    }
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PositionType that)) return false;
+        return getId() == that.getId() && Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getValue());
+    }
 
     // Getters and Setters:
 
